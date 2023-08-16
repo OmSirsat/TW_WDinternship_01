daec7e86f3941560606d2e657c93a28fe4c57f0f
Om Sirsat <108949014+OmSirsat@users.noreply.github.com>
2023-08-16 21:22:25 +0530

    Add files via upload

---
+++ again.js
@@ -0,0 +1,4 @@
+document.getElementById("flight-option").addEventListener("click", function() {
+    const flightForm = document.getElementById("flight-form");
+    flightForm.style.display = "block";
+});
